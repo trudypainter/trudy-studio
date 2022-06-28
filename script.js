665b36d0fd45980471d47971fbb93341ffe1f6ed
trudypainter <tpainter@mit.edu>
2022-06-28 11:14:43 -0400

    adding changes:

---
+++ script.js
@@ -0,0 +1,28 @@
+// console.log("script");
+
+// let blocked = false;
+
+// function hideElm(e) {
+//   console.log("🟢 HIDE");
+//   console.log(e.target.id);
+//   document.getElementById(e.target.id + "-img").style.visibility = "hidden";
+// }
+
+// function showElm(e) {
+//   console.log("🟥 show");
+
+//   console.log(e.clientX);
+//   blocked = true;
+//   document.getElementById(e.target.id + "-img").style.visibility = "visible";
+//   document.getElementById(e.target.id + "-img").style.zIndex = "1";
+//   blocked = false;
+// }
+
+// document.getElementById("meshup").addEventListener("mouseover", (elm) => {
+//   if (!blocked) {
+//     showElm(elm);
+//   }
+// });
+// document.getElementById("meshup").addEventListener("mouseout", (elm) => {
+//   hideElm(elm);
+// });
